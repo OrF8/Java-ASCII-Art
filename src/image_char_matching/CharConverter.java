5fe7dec983b249df8e5d0edd148168fc49484753
or-forshmit8 <or.forshmit@mail.huji.ac.il>
2024-12-11 21:54:18 +0200

    Noam

--- src/image_char_matching/CharConverter.java
+++ src/image_char_matching/CharConverter.java
@@ -5,9 +5,9 @@ import java.awt.image.BufferedImage;
 
 /**
  * Inspired by, and partly copied from
- * https://github.com/korhner/asciimg/blob/95c7764a6abe0e893fae56b3b6b580e09e1de209/src/main/java/io/korhner/asciimg/image/AsciiImgCache.java
+ * <a href="https://github.com/korhner/asciimg/blob/95c7764a6abe0e893fae56b3b6b580e09e1de209/src/main/java/io/korhner/asciimg/image/AsciiImgCache.java">...</a>
  * described in the blog:
- * https://dzone.com/articles/ascii-art-generator-java
+ * <a href="https://dzone.com/articles/ascii-art-generator-java">...</a>
  * Adaptations made by Dan Nirel and again by Rachel Behar.
  * The class converts characters to a binary "image" (2D array of booleans).
  */
@@ -39,8 +39,8 @@ public class CharConverter {
         BufferedImage img = new BufferedImage(pixelsPerRow, pixelsPerRow, BufferedImage.TYPE_INT_ARGB);
         Graphics g = img.getGraphics();
         g.setFont(font);
-        int xOffset = (int)Math.round(pixelsPerRow *X_OFFSET_FACTOR);
-        int yOffset = (int)Math.round(pixelsPerRow *Y_OFFSET_FACTOR);
+        int xOffset = (int)Math.round(pixelsPerRow * X_OFFSET_FACTOR);
+        int yOffset = (int)Math.round(pixelsPerRow * Y_OFFSET_FACTOR);
         g.drawString(charStr, xOffset, yOffset);
         return img;
     }
